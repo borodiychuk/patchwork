601a539089babe2d54ad388f27dd74043f3e0b09
Andriy Borodiychuk <ab@markusweb.com>
2017-07-09 10:43:40 +0200

    Optimize shuffle pattern

--- composers/shuffle.go
+++ composers/shuffle.go
@@ -14,27 +14,26 @@ type Shuffle struct{}
 // Compose composes a picture out of provided set of samples
 func (c *Shuffle) Compose(canvas *m.Canvas, samples []m.Sample) error {
 	// Elements to exclude from list to avoid similar elements staying together
-	topElement := ""
-	leftElement := ""
+	forbiddenSamples := []string{}
 
 	for i := 0; i < canvas.Length*canvas.Width; i++ {
 		col, row, err := canvas.XYforIndex(i)
 		if err != nil {
 			return err
 		}
-		if row > 0 {
-			topElementIndex, err := canvas.IndexForXY(col, row-1)
-			if err != nil {
-				return err
-			}
-			topElement = canvas.Elements[topElementIndex].Sample.ID()
-		}
 		// This piece of code with cycle is not really optimal and could utilize some caching for better performance. But from other side,
 		// that is not a tool for real-time data processing, so nothing bad happens if it stays so: not so fast
 		// but semantically correct.
 		allowedSamples := []m.Sample{}
 		for _, s := range samples {
-			if s.ID() != topElement && s.ID() != leftElement {
+			allowed := true
+			for _, fs := range forbiddenSamples {
+				if s.ID() == fs {
+					allowed = false
+					break
+				}
+			}
+			if allowed {
 				allowedSamples = append(allowedSamples, s)
 			}
 		}
@@ -45,7 +44,17 @@ func (c *Shuffle) Compose(canvas *m.Canvas, samples []m.Sample) error {
 		e.Sample = allowedSamples[rand.Intn(len(allowedSamples))]
 		e.Rotation = rand.Intn(4)
 		canvas.Elements = append(canvas.Elements, e)
-		leftElement = e.Sample.ID()
+
+		forbiddenSamples = []string{e.Sample.ID()}
+		if topElementIndex, err := canvas.IndexForXY(col, row-1); err == nil {
+			forbiddenSamples = append(forbiddenSamples, canvas.Elements[topElementIndex].Sample.ID())
+		}
+		if topLeftElementIndex, err := canvas.IndexForXY(col-1, row-1); err == nil {
+			forbiddenSamples = append(forbiddenSamples, canvas.Elements[topLeftElementIndex].Sample.ID())
+		}
+		if topRightElementIndex, err := canvas.IndexForXY(col+1, row-1); err == nil {
+			forbiddenSamples = append(forbiddenSamples, canvas.Elements[topRightElementIndex].Sample.ID())
+		}
 	}
 	return nil
 }
